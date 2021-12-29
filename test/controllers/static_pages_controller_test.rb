0e96ace5ce09a15d767b9960643e3cd8de728614
Raynard Chaney <Raynardchaney@gmail.com>
2021-12-29 14:42:16 +0000

    This is a new commit for what I originally planned to be amended

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -20,4 +20,10 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
+  
+  test "should get about" do
+    get :contact
+    assert_response :success
+    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+  end
 end
