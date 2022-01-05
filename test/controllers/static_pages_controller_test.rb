76a54991073adf51327fb5d2891c1e78d7620b0b
Raynard Chaney <Raynardchaney@gmail.com>
2022-01-05 17:49:51 +0000

    fixed contact Error

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -9,19 +9,19 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
   
   test "should get help" do
-    get :help
+    get  static_pages_help_url
     assert_response :success
     assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get about" do
-    get :about
+    get  static_pages_about_url
     assert_response :success
     assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
   
   test "should get contact" do
-    get :contact
+    get  static_pages_contact_url
     assert_response :success
     assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
   end
